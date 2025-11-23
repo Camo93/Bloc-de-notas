c410da8fe5c64e7c02e524913b2df7911cf75a11
juan camilo orjuela betancur <jcorjuela@libertadores.edu.co>
2025-11-22 23:57:18 -0500

    actualizacion con bd mongoDB atlas, prueba postman y npm start check

--- backend/src/database.js
+++ backend/src/database.js
@@ -2,10 +2,12 @@ import mongoose from 'mongoose';
 
 export async function connectDB() {
   try {
+    console.log("Conectando a MongoDB con URI:", process.env.MONGO_URI);
+
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("Mongo conectado correctamente");
+
+    console.log("Mongo conectado correctamente ✅");
   } catch (error) {
     console.error("Error conectando a Mongo:", error);
   }
 }
-
--- backend/src/models/Nota.js
+++ backend/src/models/Nota.js
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose';
+
 const notaSchema = new mongoose.Schema({
   titulo: { type: String, required: true },
   contenido: { type: String, required: true },
 }, {
   timestamps: true
 });
-export default mongoose.model('Nota', notaSchema);
+
+// El tercer parámetro fuerza el nombre de la colección en Atlas
+export default mongoose.model('Nota', notaSchema, 'notas');
